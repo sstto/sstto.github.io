998bf65cd34642198e6c4c356d3eaf39dfaa400c
stto <sspjyins@snu.ac.kr>
2022-03-04 17:04:36 +0900

    Thanks to Dang Hyeona

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "J일지도 모르는 P의 여행",
-        "excerpt":"I have been having a lot of fun exploring choroplethr package created by   THis post  ","categories": [],
-        "tags": [],
-        "url": "/PtoJ/",
+        "excerpt":"I have been having a lot of fun exploring choroplethr package created by   THis post  ","categories": ["travel"],
+        "tags": ["travel"],
+        "url": "/travel/PtoJ/",
         "teaser": null
       }]
