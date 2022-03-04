6211271e86ff010d3a2a010f1c1095ffd91bf81e
stto <sspjyins@snu.ac.kr>
2022-03-04 16:39:00 +0900

    post PtoJ:

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "J일지도 모르는 P의 여행",
+        "excerpt":"I have been having a lot of fun exploring choroplethr package created by   THis post  ","categories": [],
+        "tags": [],
+        "url": "/PtoJ/",
+        "teaser": null
+      }]
